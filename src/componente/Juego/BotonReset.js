56e8bdc3922b35f416062a19edd95eb86469b448
EliasDRobles <93445911+EliasDRobles@users.noreply.github.com>
2022-10-26 22:07:10 -0300

    Pantalla GameOver
    Correción y funcionamiento de la pantalla game over y el botón de reinicio
    
    Co-Authored-By: BoccardoAnaCatalina <93290449+AnaCatalina@users.noreply.github.com>
    Co-Authored-By: ChArLeS090800 <112178322+ChArLeS090800@users.noreply.github.com>
    Co-Authored-By: estradalautaro <93290605+estradalautaro@users.noreply.github.com>
    Co-Authored-By: fab1997 <93455997+fab1997@users.noreply.github.com>

--- src/componente/Juego/BotonReset.js
+++ src/componente/Juego/BotonReset.js
@@ -2,13 +2,20 @@
     constructor(escena){
         this.escenaR = escena;
     }
-
     precargar(){
-        this.escenaR.load.imagen("boton","imagen/BotonReinicio.jpg");
-
+        
+        this.escenaR.load.spritesheet('boton','imagen/BotonReinicio.png',{frameWidth: 200 ,frameHeight: 79});
     }
     crear(){
-        this.botoninicio = this.escenaR.add.imagen(400,500,'boton').setInteractive();
+        this.botoninicio = this.escenaR.add.sprite(400,500,'boton').setInteractive();
+
+        this.botoninicio.on('pointerover', () => {
+            this.botoninicio.setFrame(1);
+        });
+
+        this.botoninicio.on('pointerout', () => {
+            this.botoninicio.setFrame(0);
+        });
 
         this.botoninicio.on('pointerdown', () => {
             this.escenaR.scene.start('Inicio')
--- src/componente/Juego/GameOver.js
+++ src/componente/Juego/GameOver.js
@@ -7,13 +7,12 @@ class Gameover extends Phaser.Scene{
         this.reset = new BotonReset(this);
     }
     preload(){
-        this.load.image("Perder" ,"imagen/game over.jpg");
-      //  this.reset.precargar();
+        this.load.image('Perder' ,"imagen/game over.jpg");
+        this.reset.precargar();
     }
     create(){
-        this.add.image(400, 300, "fondo");
-      //  this.BotonReset.crear();
         this.imagenWin = this.add.image(400,300,'Perder');
+        this.reset.crear();
     }
 }
 export default Gameover;
