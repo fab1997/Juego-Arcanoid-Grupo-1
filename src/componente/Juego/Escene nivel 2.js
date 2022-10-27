df3d9de10bec9f6295064e029ebad0d67f028eb1
ChArLeS090800 <jf964815@gmail.com>
2022-10-27 09:25:41 -0300

    Nivel2
    cambie el fondo, la musica, y el color de los bloques para el nivel 2, espero este bien :(
    
    Co-Authored-By: BoccardoAnaCatalina <93290449+AnaCatalina@users.noreply.github.com>
    Co-Authored-By: EliasDRobles <93445911+EliasDRobles@users.noreply.github.com>
    Co-Authored-By: estradalautaro <93290605+estradalautaro@users.noreply.github.com>
    Co-Authored-By: fab1997 <93455997+fab1997@users.noreply.github.com>

---
+++ src/componente/Juego/Escene nivel 2.js
@@ -0,0 +1,146 @@
+import Phaser from "phaser";
+
+class Escene extends Phaser.Scene {
+    constructor() {
+        super({ key: 'Inicio' });
+    }
+
+    //Init(){
+    // this.puntos = 0;   
+    //}
+
+    //Se emplean variables globales
+    plataforms = null;
+    cursors = null;
+    puntaje = 0;
+    puntos = 10;
+    sonido1 = null;
+
+    preload() {
+        this.load.image("fondo", "imagen/fondo2.png");
+        this.load.image("base", "imagen/paddle1.png");
+        this.load.image("bloque", "imagen/green.png");
+        this.load.image("bloque", "imagen/orange.jpg");
+        this.load.image("bloque", "imagen/purple1.png");
+        this.load.image("ball", "imagen/ball1.png");
+        this.load.audio('nivel2', 'sonido/level2.mp3')
+
+    }
+    create() {
+        //creando el fondo
+        this.add.image(400, 300, "fondo2");
+
+        this.sonido1 = this.sound.add('nivel2');
+        const soundConfig = {
+            loop: true
+        }
+
+        this.sonido1.play(soundConfig)
+
+        this.puntajeEnTexto = this.add.text(10, 10, 'Puntos: 0', {
+            fontSize: '20px',
+            fill: 'red',
+            fontFamily: 'arial'
+        });
+
+
+
+        //se crea los bloques
+        this.bloques = this.physics.add.staticGroup({
+            key: ['bloqueVerde', 'bloqueNaranja', 'bloqueVioleta'],
+            frameQuantity: 1,
+            gridAlign: { width: 10, height: 10, cellWidth: 64, cellHeight: 32, x: 112, y: 50 }
+        });
+
+        // se crea la pelota con sus fisicas
+        this.ball = this.physics.add.image(400, 480, "ball").setCollideWorldBounds(true).setBounce(1);
+        this.ball.setData('apagada', true);
+
+        const keyCodes = Phaser.Input.Keyboard.KeyCodes;
+        this.teclaA = this.input.keyboard.addKey(keyCodes.A);
+
+        // se crea la platafomra
+        this.plataforms = this.physics.add.image(400, 500, "base").setImmovable();
+        // se cancela la gravedad
+        this.plataforms.body.allowGravity = false;
+        this.plataforms.setCollideWorldBounds(true);
+        // se agrega un objeto para mover la plataforma
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+
+
+
+        this.physics.add.collider(this.ball, this.plataforms, this.contarColision, null, this);
+        this.physics.add.collider(this.ball, this.bloques, this.colisionPelotaBloque, null, this);
+
+        this.physics.world.setBoundsCollision(true, true, true, false);
+
+
+    }
+
+    //Método que detecta colisión entre la pelota y los bloques
+    colisionPelotaBloque(ball, bloques) {
+        bloques.disableBody(true, true);
+        this.aumentarPuntaje();
+        /* if(this.bloques.countActive()===0){
+             this.felicitar();
+         }
+        }
+     felicitar(){
+         this.scene.start("Win");
+ 
+     }*/
+    }
+
+    //Método que permite aumentar el puntaje
+    aumentarPuntaje() {
+        this.puntaje = this.puntaje + this.puntos;
+        this.puntajeEnTexto.setText('Puntos: ' + this.puntaje);
+        console.log(this.puntaje);
+    }
+
+    //Método que detecta colisión entre la pelota y la plataforma
+    contarColision(ball, plataforms) {
+        let lugarDeImpacto = ball.x - plataforms.x
+        if (lugarDeImpacto > 0) {
+            ball.setVelocityX(2 * lugarDeImpacto);
+        } else if (lugarDeImpacto < 0) {
+            ball.setVelocityX(2 * lugarDeImpacto);
+        } else {
+            ball.setVelocityX(Phaser.Math.Between(-10, 10))
+        }
+    }
+    update() {
+        //Movimientos laterales de la plataforma
+        if (this.cursors.left.isDown) {
+            this.plataforms.setVelocityX(-300);
+
+        }
+        else if (this.cursors.right.isDown) {
+            this.plataforms.setVelocityX(300);
+
+        }
+        else {
+            this.plataforms.setVelocityX(0);
+
+        }
+        if (this.ball.getData('apagada')) {
+            this.ball.x = this.plataforms.x;
+
+        }
+        if (this.cursors.space.isDown && this.ball.getData('apagada', true)) {
+            this.ball.setVelocity(50, -450);
+            this.ball.setData('apagada', false);
+        }
+        if (this.ball.y > 600) {
+            this.sonido1.stop();
+            this.mostrarGameover();
+        }
+    }
+
+    mostrarGameover() {
+        this.scene.start('GameOver')
+    }
+}
+
+export default Escene;
